38272fc4a9e73d7587dfffd548d4dec35dd875c3
Emil Borowiec <emil@ponderingprogrammer.com>
2020-08-06 22:48:24 +0200

    Fixed test

--- GridMath/tests/PonderingProgrammer.GridMath.Tests/GridBoundingBoxTest.cs
+++ GridMath/tests/PonderingProgrammer.GridMath.Tests/GridBoundingBoxTest.cs
@@ -26,8 +26,8 @@ namespace PonderingProgrammer.GridMath.Tests
         [Fact]
         public void TestPlaceBeside()
         {
-            var box1 = GridBoundingBox.FromMinMax(10, 5, 20, 15);
-            var box2 = GridBoundingBox.FromMinMax(0, 0, 2, 4);
+            var box1 = GridBoundingBox.FromMinMaxExcl(0, 0, 1, 3);
+            var box2 = GridBoundingBox.FromMinMaxExcl(10, 5, 20, 15);
             var onTop = box1.PlaceBeside(box2, Grid4Direction.Top);
             var onRight = box1.PlaceBeside(box2, Grid4Direction.Right);
             var under = box1.PlaceBeside(box2, Grid4Direction.Bottom);
@@ -36,7 +36,7 @@ namespace PonderingProgrammer.GridMath.Tests
             Assert.Equal(14, onTop.MinX);
             Assert.Equal(5, onTop.MaxYExcl);
             Assert.Equal(14, under.MinX);
-            Assert.Equal(20, under.MinY);
+            Assert.Equal(15, under.MinY);
             Assert.Equal(20, onRight.MinX);
             Assert.Equal(10, onLeft.MaxXExcl);
         }
